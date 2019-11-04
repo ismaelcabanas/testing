6c8a4489b7105b89449c2a7a0954fe8d620261bc
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-11-04 11:27:09 +0100

    Add wiremock testing example project

---
+++ testing-springboot-wiremock/src/main/java/cabanas/garcia/ismael/testing/testingspringbootwiremock/TestingSpringbootWiremockApplication.java
@@ -0,0 +1,13 @@
+package cabanas.garcia.ismael.testing.testingspringbootwiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSpringbootWiremockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSpringbootWiremockApplication.class, args);
+	}
+
+}
---
+++ testing-springboot-wiremock/src/test/java/cabanas/garcia/ismael/testing/testingspringbootwiremock/TestingSpringbootWiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package cabanas.garcia.ismael.testing.testingspringbootwiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringbootWiremockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
